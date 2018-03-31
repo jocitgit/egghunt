a95a727492e0cf42063ee1b70d68c3d854b86aa0
Joella Flood <joella.flood@mycit.ie>
2018-03-31 14:53:54 +0100

    Updated clues and codes

--- app/src/main/java/ie/cit/soft7035/egghunt/Code.java
+++ app/src/main/java/ie/cit/soft7035/egghunt/Code.java
@@ -7,14 +7,14 @@ package ie.cit.soft7035.egghunt;
 public class Code {
 
     public static final String[] codes = {
-            "code1",
-            "code2",
-            "code3",
-            "code4",
-            "code5",
-            "code6",
-            "code7",
-            "code8"};
+            "table",
+            "nothing",
+            "stick",
+            "carrot",
+            "towel",
+            "short",
+            "clock",
+            "fiiish"};
 
     public static final String[][] clues = {
             {"Play this if you're feeling bored,", "A game where tiny goals are scored."},
@@ -22,7 +22,7 @@ public class Code {
             {"Park your car and look around,", "Who knows what's lurking on the ground?"},
             {"You must be careful not to trip,", "When looking for a place to zip."},
             {"Hubble bubble, toil and trouble,", "Bathe outside and watch it bubble!"},
-            {"This gazebo's painted blue,", "Find it for another clue."},
+            {"This outdoor seat is painted blue,", "Find it for another clue."},
             {"The Easter Bunny likes his greens,", "But where would he grind coffee beans?"},
             {"99 bottles of beer on the wall,", "I hope the fish don't drink it all!"}};
 
